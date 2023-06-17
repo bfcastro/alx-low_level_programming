99e0040eb3eb9a7f157e4e77eb977eb69af5ed3c
bfcastro <baffourcastro@gmail.com>
2023-06-16 17:33:32 -0700

    Base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,16 @@
   */
 int main(void)
 {
-	int digit = 48;
+	int n;
+	int m;
 
-	while (digit <= 102)
+for (n = 48; n <= 57; n++)
 	{
-		putchar(digit);
-
-	if (digit == 57)
-		digit += 39;
-		digit++;
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
 
 	}
 			putchar('\n');
