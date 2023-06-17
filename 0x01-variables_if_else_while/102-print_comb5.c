b2be626d9112656e4dbb3eaabecff3bea10ab23f
bfcastro <baffourcastro@gmail.com>
2023-06-16 20:49:33 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,28 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
   * main - Entry point
   * Return: 0
   */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int p, q;
 
-	int c2;
-	int f_d2;
-	int l_d2;
-
-	while (c <= 98)
+	for (p = 0; p<= 98; p++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 = 0;
-		while (c2 <= 99)
+		for (q = p + 1, q <= 99; q++)
 		{
-			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
-
-			if (c < c2)
-			{
-				putchar(f_d);
-				putchar(l_d);
-				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-
-				if (c != 98)
-				{
-					putchar(' ');
-					putchar(',');
-				}
-			}
-			c2++;
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+			if (p == 98 && q == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-		c++;
 	}
 	putchar('\n');
 	return (0);
