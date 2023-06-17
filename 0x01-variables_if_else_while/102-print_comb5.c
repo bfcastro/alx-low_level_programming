f51836d91dc3be9cc15b470a5d3d8d1c08dbbd4d
bfcastro <baffourcastro@gmail.com>
2023-06-16 21:06:13 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -11,7 +11,7 @@ int main(void)
 
 	for (p = 0; p<= 98; p++)
 	{
-		for (q = p + 1, q <= 99; q++)
+		for (q = p + 1; q <= 99; q++)
 		{
 			putchar((p / 10) + '0');
 			putchar((p % 10) + '0');
