1802fe7e3858144f68be8d296e6a99b9089b37a8
bfcastro <baffourcastro@gmail.com>
2023-06-16 18:42:42 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  * main - main block
-  * Description: Write a program that prints all possible
-  * Different combinations of two digits
-  * Numbers must be separated by ,, followed by a space
-  * The two digits must be different
-  * 01 and 10 are considered the same combination of the two digits 0 and 1
-  * Print only the smallest combination of two digits
-  * Numbers should be printed in ascending order, with two digits
-  * You can only use the putcher function
-  * (every other function (printf, puts, etc...) is forbidden)* You can only use putcher five times maximum in your code
-  * You are not allowed to use any variable of type char
-  * All your code should be in the main function
+  * main - Entry point here
   * Return: 0
   */
 int main(void)
