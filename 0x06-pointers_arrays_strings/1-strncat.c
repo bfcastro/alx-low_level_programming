4d8ff9865992096c9d571340c138b5ae29d20288
bfcastro <baffourcastro@gmail.com>
2023-06-29 16:14:41 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,6 @@
   */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
 	
 	int index = 0, dest_len = 0;
@@ -25,4 +24,5 @@ char *_strncat(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
+
 }
