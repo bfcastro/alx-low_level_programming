9af15c04bf09780163817f3fb838a1333c9e700c
bfcastro <baffourcastro@gmail.com>
2023-06-30 09:59:46 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,11 +4,10 @@
   * _strcmp - Compares pointers to two strings
   * @s1: First string pointer to be compared
   * @s2: Second string pointer to be compared
-  * Return: If str1 < str2, the negative difference of the first unmatched characters
+  * Return: If str1 < str2, the negative difference of the first
   * If str1 == str2, 0
   * If str1 > str2, the positive differnce of the first unmatched characters
   */
-
 int _strcmp(char *s1, char *s2)
 
 {
@@ -18,5 +17,4 @@ int _strcmp(char *s1, char *s2)
 	s2++;
 	}
 	return (*s1 - *s2);
-
 }
