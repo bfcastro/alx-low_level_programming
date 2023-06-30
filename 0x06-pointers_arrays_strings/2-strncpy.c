1c904036772ffbe7c0ec48c3109e872997f228a1
bfcastro <baffourcastro@gmail.com>
2023-06-30 10:13:18 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,7 @@
  #include "main.h"
-   
+
+
+
  /**
   * _strncpy - Copies at most an inputted number
   * of bytes from string src into dest.
@@ -17,13 +19,13 @@
          int index = 0, src_len = 0;
           
          while (src[index++])
-                  src_len++;
+         src_len++;
           
          for (index = 0; src[index] && index < n; index++)
-                  dest[index] = src[index];
+         dest[index] = src[index];
           
          for (index = src_len; index < n; index++)
-                  dest[index] = '\0';
+         dest[index] = '\0';
 
 	 return (dest);
 
