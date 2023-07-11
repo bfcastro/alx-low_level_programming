c2b16c4d1081408d25831cee6ba58867184579be
bfcastro <baffourcastro@gmail.com>
2023-07-11 03:42:55 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - frees the previous grid created
+  * @grid: previous grid
+  * @height: height of grid
+  *
+  * Return: Nothing
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
