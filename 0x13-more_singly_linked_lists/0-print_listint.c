59af7a2dfca3a13a304ea4a459cbc6b41197bcb8
bfcastro <baffourcastro@gmail.com>
2023-08-01 05:22:20 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,10 @@
 /**
   * print_listint - prints all the elements of a linked list
   * @h: linked list of type listint_t to print
+  * 
   * Return: number of nodes
   */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
@@ -15,5 +17,6 @@ size_t print_listint(const listint_t *h)
 		num++;
 		h = h->next;
 	}
+
 	return (num);
 }
